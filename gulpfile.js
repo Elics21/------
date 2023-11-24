8aebf68ffcc8cda02b0a585b5ebf079068a97ce7
Elics21 <paw.shweczow@yandex.ru>
2023-11-24 20:27:36 +0700

    eddit gulfile (copy: img, fonts, icons. html)

--- gulpfile.js
+++ gulpfile.js
@@ -29,22 +29,40 @@ gulp.task("build:css", function () {
     return gulp
         .src("src/css/**/*.css")
         .pipe(cleanCSS())
-        .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest("dist/css"));
 });
 
 gulp.task("build:js", function () {
-    return gulp
-        .src("src/js/**/*.js")
-        .pipe(uglify())
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest("dist/js"));
+    return gulp.src("src/js/**/*.js").pipe(uglify()).pipe(gulp.dest("dist/js"));
 });
 
 gulp.task("copy:html", function () {
     return gulp.src("src/*.html").pipe(gulp.dest("dist"));
 });
 
-gulp.task("build", gulp.series("sass", "build:css", "build:js", "copy:html"));
+gulp.task("copy:img", function () {
+    return gulp.src("src/img/**/*").pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("copy:icons", function () {
+    return gulp.src("src/icons/**/*").pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task("copy:fonts", function () {
+    return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task(
+    "build",
+    gulp.series(
+        "sass",
+        "build:css",
+        "build:js",
+        "copy:html",
+        "copy:img",
+        "copy:icons",
+        "copy:fonts"
+    )
+);
 
 gulp.task('default', gulp.parallel('server', 'sass'));
